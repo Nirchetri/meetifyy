8be7f98ae32ca6d156ee9f2d8d8c94f6e52b728a
Your Name <nikhilchetry942@gmail.com>
2025-03-20 20:16:03 +0530

    updated version

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -9,10 +9,10 @@ const Navbar = () => {
     <nav className="flex-between fixed z-50 w-full bg-dark-1 px-6 py-4 lg:px-10">
       <Link href="/" className="flex items-center gap-1">
         <Image
-          src="/icons/logo.svg"
+          src="/icons/yoom-logo.svg"
           width={32}
           height={32}
-          alt="yoom logo"
+          alt="meetify logo"
           className="max-sm:size-10"
         />
         <p className="text-[26px] font-extrabold text-white max-sm:hidden">
