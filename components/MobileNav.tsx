c754f7d513cfe50ce6c2c7b5374b014c89719c9d
Your Name <nikhilchetry942@gmail.com>
2025-03-20 20:12:33 +0530

    refine

--- components/MobileNav.tsx
+++ components/MobileNav.tsx
@@ -31,7 +31,7 @@ const MobileNav = () => {
               height={32}
               alt="meetify logo"
             />
-            <p className="text-[26px] font-extrabold text-white">Meetify</p>
+            <p className="text-[26px] font-extrabold text-white">eetify</p>
           </Link>
           <div className="flex h-[calc(100vh-72px)] flex-col justify-between overflow-y-auto">
             <SheetClose asChild>
